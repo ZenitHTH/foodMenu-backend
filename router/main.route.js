38e55a6bd8af249d11820017b19f732f4a3d6964
settawat boriruklert <z.z.z.z.z.z.z.zenith@gmail.com>
2022-07-25 16:36:52 +0700

    fixed bug just get a last data

--- router/main.route.js
+++ router/main.route.js
@@ -7,39 +7,49 @@ const typeShema = require("../models/type.db");
 const subtypeShema = require("../models/subtype.db");
 const imageShema = require("../models/image.db");
 
-router.route("/").get((req, res) => {
-  //get allfoodlist with objectid
-  foodShema.find({}, async (err, data) => {
-    //set empty array waiting push a data
-    const subtypeList = [];
+router.route("/").get(async (req, res) => {
+  const data = await foodShema.find({});
+  const pushData = async (data, done) => {
     const foodList = [];
-    if (err) console.log(err);
-    //push subtype's name into subtypelist
-    const fetchData = await data.map(async (f) => {
-      console.log(f);
-      await f.idsubtype.map(async (id) => {
-        const subtype = await subtypeShema.findById(id);
+    Promise.all(
+      await data.map(async (f) => {
+        const subtypeList = [];
+        await f.idsubtype.map(async (id) => {
+          const subtype = await subtypeShema.findById(id);
 
-        subtypeList.push(subtype.name);
-      });
+          subtypeList.push(subtype.name);
+        });
+        const type = await typeShema.findById(f.idtype);
+        const image = await imageShema.findById(f.idimage);
 
-      // push food data's name into foodlist
-      const type = await typeShema.findById(f.idtype);
-      const image = await imageShema.findById(f.idimage);
-      foodList.push({
-        name: f.name,
-        price: f.price,
-        type: type.name,
-        subtype: subtypeList,
-        image: image.img,
-      });
+        foodList.push({
+          name: f.name,
+          price: f.price,
+          type: type.name,
+          subtype: subtypeList,
+          image: image.img,
+        });
+      })
+    ).then(() => {
+      done(null, foodList);
     });
-    //show data food without objectid
-    Promise.all(fetchData).then(() => {
+  };
+
+  pushData(data, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(result);
+      res.status(201).end();
+    }
+  });
+
+  /*.exec()
+    .then(() => {
+      console.log(foodList);
       res.json(foodList);
       res.status(201).end();
-    });
-  });
+    });*/
 });
 
 module.exports = router;
