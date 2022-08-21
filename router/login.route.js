6bc82ae743ddeeada6e5568a8bf14aeb7b28b7cb
ZenitHTH <z.z.z.z.z.z.z.zenith@gmail.com>
2022-08-21 16:42:48 +0700

    add login (dosen't test)

--- router/login.route.js
+++ router/login.route.js
@@ -1,17 +1,30 @@
 const mongoose = require("mongoose")
 const express = require("express");
 const fs = require("fs");
+const jet = require("jsonwebtoken")
+require("dotenv").config
 
 const router = express.Router();
 const path = require("path");
 const userSchema = require("../models/user.db");
 
-router.route("/").post((req,res)=>{
+router.route("/").post(async (req,res)=>{
     const { username,password } = req.body;
-    if(!userSchema.findOne({username})){
-        console.log("dont have this username")
+    const user = await userSchema.findOne({username}); 
+    if(!user){
+        res.status(400).send("not found username").end();
     }else{
-        console.log("already have this username")
+        const token = await jwt.sign(
+            {
+                user_id:user._id,username 
+            },
+            process.env.TOKEN_KEY,
+            {
+                expiresIn:"1h"
+            }
+        )
+        user.token=token;
+        res.json(user).status(200).end();
     }
     res.status(201).end()
 
