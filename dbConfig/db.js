04bfb4ed71a902317869b9f37815a71e5ab708c5
ZenitHTH <z.z.z.z.z.z.z.zenith@gmail.com>
2022-08-21 22:26:12 +0700

    add login system

--- dbConfig/db.js
+++ dbConfig/db.js
@@ -1,7 +1,6 @@
 require("dotenv").config()
 
 module.exports = {
-
   db: `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.gtalbjy.mongodb.net/foodMenu?retryWrites=true&w=majority`,
   localdb:"mongodb://localhost:27017/foodMenu"
 };
--- router/login.route.js
+++ router/login.route.js
@@ -1,33 +1,46 @@
-const mongoose = require("mongoose")
 const express = require("express");
 const fs = require("fs");
-const jet = require("jsonwebtoken")
-require("dotenv").config
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+require("dotenv").config;
 
 const router = express.Router();
 const path = require("path");
 const userSchema = require("../models/user.db");
 
-router.route("/").post(async (req,res)=>{
-    const { username,password } = req.body;
-    const user = await userSchema.findOne({username}); 
-    if(!user){
-        res.status(400).send("not found username").end();
-    }else{
-        const token = await jwt.sign(
+router.route("/").post(async (req, res) => {
+  const { username, password } = req.body;
+  const user = await userSchema.findOne({ username });
+  if (!user) {
+    res.status(400).send("not found username").end();
+  } else {
+    if (password.length > 0) {
+      bcrypt.compare(password, user.passwordhash, async (error, result) => {
+        if (error) {
+          console.error("bcrypt compare error", error);
+        }
+        if (result === true) {
+          const token = await jwt.sign(
             {
-                user_id:user._id,username 
+              user_id: user._id,
+              username,
             },
             process.env.TOKEN_KEY,
             {
-                expiresIn:"1h"
+              expiresIn: "1h",
             }
-        )
-        user.token=token;
-        res.json(user).status(200).end();
+          );
+
+          user.token = token;
+          res.status(200).json({ user });
+        } else {
+           res.send("missmatch password").status(201).end();
+        }
+      });
+    } else {
+        res.send("Password is empty").status(201).end()
     }
-    res.status(201).end()
+  }
+});
 
-})
-
-module.exports = router
+module.exports = router;
