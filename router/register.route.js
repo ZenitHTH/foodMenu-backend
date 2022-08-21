3b772d28845d6bb1c9c01e8970d967832c1afffd
ZenitHTH <z.z.z.z.z.z.z.zenith@gmail.com>
2022-08-21 16:43:09 +0700

    register can hash password but not give token

--- router/register.route.js
+++ router/register.route.js
@@ -1,37 +1,51 @@
 const mongoose = require("mongoose");
 const express = require("express");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const fs = require("fs");
 const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 const path = require("path");
 const userSchema = require("../models/user.db");
-require("dotenv").config()
+const { resolve } = require("path");
+require("dotenv").config();
 
 router.route("/").post(async (req, res) => {
   const { username, password } = req.body;
   try {
-    const user = await userSchema.findOne({ username:username });
-    const passwordhash = bcrypt.genSalt(10,(err,salt)=>{
-        if(err) console.log(err)
-        return bcrypt.hash(password,salt,(err,hash)=>hash);
-    })
+    const user = await userSchema.findOne({ username: username });
+    console.log(user)
     if (!user) {
-      const newUser = await userSchema.create({
-        username:username,
-        passwordhash: passwordhash
+      bcrypt.genSalt(10, (error, salt) => {
+        if (error) {
+          console.error("GenSalt Error", error);
+        }
+        console.log(`salt:${salt}`)
+        bcrypt.hash(password, salt, async (error, hash) => {
+          if (error) {
+            console.error("HashPassword Error", error);
+          }
+          console.log(`hash:${hash}`)
+          const newUser = await userSchema.create({
+            username: username,
+            passwordhash: hash
+          });
+          console.log(`newUser:${newUser}`)
+          const token = await jwt.sign(
+            { user_id: newUser._id, username },
+            process.env.TOKEN_KEY,
+            { expiresIn: "1h" }
+          );
+          console.log(token)
+          newUser.token = token;
+          res.status(200).json({username:newUser.username,_id:newUser._id}).end();
+        });
       });
-      const token = await jwt.sign(
-        { user_id: newUser._id, username },
-        process.env.TOKEN_KEY,
-        { expiresIn: "1h" }
-      );
-      newUser.token = token;
-      res.json(newUser).status(200).end();
     } else {
-      console.log("already have this username");
-      res.status(400).send("Duplicate Username").end();
+      if(!username) console.log("forgot username");
+      if(!password) console.log("forgot password");
+      
+      res.status(400).send("Register have problem").end();
     }
   } catch (err) {
     console.log(err);
